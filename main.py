65703de0d623d5fc3fa2fba85c0844a62fc920d4
Gfarf <gfpalu@gmail.com>
2025-03-30 08:44:46 -0300

    desenhando o jogador

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     oe = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    nave = Player(x=SCREEN_WIDTH /2, y=SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        nave.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) /1000
 
 
-    print(oe)
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
